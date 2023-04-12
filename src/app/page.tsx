58a931acd91de2af14d5f346be6f6b0db78e1efb
Matteo Kuberek <matteokuberek@gmail.com>
2023-04-12 15:59:49 -0400

    Add hello world

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,8 +10,8 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
-
-    </>
+    <div className={inter.className}>
+      Hello World
+    </div>
   )
 }
